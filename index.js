9a9b69db3b4e7363dcaa0ea59a2172451c56d19f
Mercy <mercycheruiyot2013@gmail.com>
2022-08-30 17:15:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+
 // code your solution here
+
+  const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  function superbowlWin(records){
+      for (const record of records) {
+        if (record.result === "W") {
+          return record.year;
+        }
+      }
+    }
+   /* if (record.result == "W"){
+      return record
+    }
+   else{
+    return undefined;
+   };
+  }
+  console.log(record.find(superbowlWin));
+  */
+  //const sadReality=record.find(({ result }) => result != 'W');
+  //console.log(sadReality);
+  //console.log(record.find(superbowlWin => superbowlWin.result==="W"));
+
+  
